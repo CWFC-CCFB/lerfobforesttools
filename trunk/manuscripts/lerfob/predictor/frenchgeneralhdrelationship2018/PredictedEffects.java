d55d91fae0282e72499b2aef7c023e91f983b54f
donenrique <donenrique@3e30b2ba-f276-471b-a5e6-14e64de59c1a>
2018-02-12 09:45:04 +0000

    New output from the HD relationships for manuscrit writing

--- trunk/manuscripts/lerfob/predictor/frenchgeneralhdrelationship2018/PredictedEffects.java
+++ trunk/manuscripts/lerfob/predictor/frenchgeneralhdrelationship2018/PredictedEffects.java
@@ -69,9 +69,11 @@ public class PredictedEffects {
 		private final double plotAreaHa = 15d * 15d * Math.PI * .0001;
 		private double basalAreaM2Ha = 25d;
 		
-		private double meanTemperatureGrowingSeason;
-		private double meanPrecipitationGrowingSeason;
+		private double meanTemperatureGrowingSeason = 13;
+		private double meanPrecipitationGrowingSeason = 600;
 		private final List<FrenchHDRelationship2018Tree> trees;
+		private double meanQuadraticDiameterCm = 20d;
+		private double slopeInclination = 0;
 		
 		Stand() {
 			trees = new ArrayList<FrenchHDRelationship2018Tree>();
@@ -88,7 +90,7 @@ public class PredictedEffects {
 		public int getMonteCarloRealizationId() {return 0;}
 
 		@Override
-		public double getMeanQuadraticDiameterCm() {return 20d;}
+		public double getMeanQuadraticDiameterCm() {return meanQuadraticDiameterCm;}
 
 		@Override
 		public boolean isInterventionResult() {return false;}
@@ -101,7 +103,7 @@ public class PredictedEffects {
 		private double getPlotAreaHa() {return plotAreaHa;}
 
 		@Override
-		public double getSlopePercent() {return 0;}
+		public double getSlopePercent() {return slopeInclination;}
 
 		@Override
 		public Collection<FrenchHDRelationship2018Tree> getTreesForFrenchHDRelationship() {return trees;}
@@ -129,7 +131,7 @@ public class PredictedEffects {
 	
 	
 
-	public static void main(String[] args) throws IOException {
+	private void testTemperatureRange() throws IOException {
 		Map<FrenchHd2018Species, Range> rangeMap = new HashMap<FrenchHd2018Species, Range>();
 		Object[] record;
 		String filenameTempRange = ObjectUtility.getPackagePath(PredictedEffects.class) + "tempRange.csv";
@@ -151,12 +153,8 @@ public class PredictedEffects {
 		FrenchHDRelationship2018Predictor predictor = new FrenchHDRelationship2018Predictor();
 		double dbhCm = 20;
 		
-		
-		
-		
-		
 		Map<FrenchHd2018Species, FrenchHDRelationship2018InternalPredictor> internalPredictorMap = predictor.getInternalPredictorMap();
-		String filename = ObjectUtility.getPackagePath(PredictedEffects.class) + "tempEffectPred.csv";
+		String filename = ObjectUtility.getPackagePath(PredictedEffects.class) + "EffectPredTemp.csv";
 		filename = filename.replace("bin", "manuscripts");
 		CSVWriter writer = null;
 		try {
@@ -203,9 +201,166 @@ public class PredictedEffects {
 				writer.close();
 			}
 		}
-
 	}
 
+	
+	
+	private void testPrecipitationRange() throws IOException {
+		Map<FrenchHd2018Species, Range> rangeMap = new HashMap<FrenchHd2018Species, Range>();
+		Object[] record;
+		String filenameTempRange = ObjectUtility.getPackagePath(PredictedEffects.class) + "precRange.csv";
+		CSVReader reader = new CSVReader(filenameTempRange);
+		while ((record = reader.nextRecord()) != null) {
+			String speciesName = record[0].toString();
+			speciesName = speciesName.replaceAll("-", " ");
+			speciesName = speciesName.replaceAll("'", " ");
+			FrenchHd2018Species species = FrenchHd2018Species.valueOf(speciesName.toUpperCase().replace(" ", "_"));
+			double mean = Double.parseDouble(record[1].toString());
+			double min = Double.parseDouble(record[2].toString());
+			double max = Double.parseDouble(record[3].toString());
+			Range r = new Range(mean, min, max);
+			rangeMap.put(species, r);
+		}
+		reader.close();
+		
+		
+		FrenchHDRelationship2018Predictor predictor = new FrenchHDRelationship2018Predictor();
+		double dbhCm = 20;
+		
+		Map<FrenchHd2018Species, FrenchHDRelationship2018InternalPredictor> internalPredictorMap = predictor.getInternalPredictorMap();
+		String filename = ObjectUtility.getPackagePath(PredictedEffects.class) + "EffectPredPrec.csv";
+		filename = filename.replace("bin", "manuscripts");
+		CSVWriter writer = null;
+		try {
+			writer = new CSVWriter(new File(filename), false);
+			List<FormatField> fields = new ArrayList<FormatField>();
+			fields.add(new CSVField("species"));
+			fields.add(new CSVField("dbhCm"));
+			fields.add(new CSVField("basalAreaM2Ha"));
+			fields.add(new CSVField("inclination"));
+			fields.add(new CSVField("meanTemp"));
+			fields.add(new CSVField("meanPrec"));
+			fields.add(new CSVField("reference"));
+			fields.add(new CSVField("pred"));
+			fields.add(new CSVField("var"));
+			writer.setFields(fields);
+
+			for (FrenchHd2018Species species : FrenchHd2018Species.values()) {
+				if (internalPredictorMap.get(species).hasPrecipitationEffect()) {
+					Tree t = new Tree(species, dbhCm);
+					Stand s = new Stand();
+					Range r = rangeMap.get(species);
+					double range = r.max - r.min;
+					double meanPrecipitation = r.mean;
+					
+					s.meanPrecipitationGrowingSeason = meanPrecipitation;
+					GaussianEstimate prediction = internalPredictorMap.get(species).predictHeightAndVariance(s, t);
+					t.reference = 1;
+					writeRecord(s, t, prediction, writer);
+					
+					double step = range * .01;
+					for (double prec = r.min; prec <= r.max; prec += step) {
+						s.meanPrecipitationGrowingSeason = prec;
+						t.reference = 0;
+						prediction = internalPredictorMap.get(species).predictHeightAndVariance(s, t);
+						writeRecord(s, t, prediction, writer);
+					}
+				}
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (writer != null) {
+				writer.close();
+			}
+		}
+	}
+
+	
+	private void testBasalAreaRange() throws IOException {
+		FrenchHDRelationship2018Predictor predictor = new FrenchHDRelationship2018Predictor();
+		double dbhCm = 20;
+		
+		Map<FrenchHd2018Species, FrenchHDRelationship2018InternalPredictor> internalPredictorMap = predictor.getInternalPredictorMap();
+		String filename = ObjectUtility.getPackagePath(PredictedEffects.class) + "EffectPredBasalArea.csv";
+		filename = filename.replace("bin", "manuscripts");
+		CSVWriter writer = null;
+		try {
+			writer = new CSVWriter(new File(filename), false);
+			List<FormatField> fields = new ArrayList<FormatField>();
+			fields.add(new CSVField("species"));
+			fields.add(new CSVField("dbhCm"));
+			fields.add(new CSVField("basalAreaM2Ha"));
+			fields.add(new CSVField("inclination"));
+			fields.add(new CSVField("meanTemp"));
+			fields.add(new CSVField("meanPrec"));
+			fields.add(new CSVField("reference"));
+			fields.add(new CSVField("pred"));
+			fields.add(new CSVField("var"));
+			writer.setFields(fields);
+
+			GaussianEstimate prediction;
+			for (FrenchHd2018Species species : FrenchHd2018Species.values()) {
+				Tree t = new Tree(species, dbhCm);
+				Stand s = new Stand();
+				for (double basalArea = 10; basalArea <= 40; basalArea += .5) {
+					s.basalAreaM2Ha = basalArea;
+					prediction = internalPredictorMap.get(species).predictHeightAndVariance(s, t);
+					writeRecord(s, t, prediction, writer);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (writer != null) {
+				writer.close();
+			}
+		}
+	}
+
+	private void testSlopeInclinationRange() throws IOException {
+		FrenchHDRelationship2018Predictor predictor = new FrenchHDRelationship2018Predictor();
+		double dbhCm = 20;
+		
+		Map<FrenchHd2018Species, FrenchHDRelationship2018InternalPredictor> internalPredictorMap = predictor.getInternalPredictorMap();
+		String filename = ObjectUtility.getPackagePath(PredictedEffects.class) + "EffectPredSlope.csv";
+		filename = filename.replace("bin", "manuscripts");
+		CSVWriter writer = null;
+		try {
+			writer = new CSVWriter(new File(filename), false);
+			List<FormatField> fields = new ArrayList<FormatField>();
+			fields.add(new CSVField("species"));
+			fields.add(new CSVField("dbhCm"));
+			fields.add(new CSVField("basalAreaM2Ha"));
+			fields.add(new CSVField("inclination"));
+			fields.add(new CSVField("meanTemp"));
+			fields.add(new CSVField("meanPrec"));
+			fields.add(new CSVField("reference"));
+			fields.add(new CSVField("pred"));
+			fields.add(new CSVField("var"));
+			writer.setFields(fields);
+
+			GaussianEstimate prediction;
+			for (FrenchHd2018Species species : FrenchHd2018Species.values()) {
+				Tree t = new Tree(species, dbhCm);
+				Stand s = new Stand();
+				for (double slope = 0; slope <= 70; slope += 1) {
+					s.slopeInclination = slope;
+					prediction = internalPredictorMap.get(species).predictHeightAndVariance(s, t);
+					writeRecord(s, t, prediction, writer);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (writer != null) {
+				writer.close();
+			}
+		}
+	}
+
+	
 
 	private static void writeRecord(Stand stand, Tree tree, GaussianEstimate prediction, CSVWriter writer) throws IOException {
 		Object[] record = new Object[9];
@@ -224,7 +379,13 @@ public class PredictedEffects {
 	}
 
 
-
+	public static void main(String[] args) throws IOException {
+		PredictedEffects p = new PredictedEffects();
+		p.testTemperatureRange();
+		p.testPrecipitationRange();
+		p.testBasalAreaRange();
+		p.testSlopeInclinationRange();
+	}
 
 
 }
