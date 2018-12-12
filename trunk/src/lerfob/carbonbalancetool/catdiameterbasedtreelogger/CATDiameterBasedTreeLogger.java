57fc760948bd913c7d468dd6765fb2c0ac38d81b
donenrique <donenrique@3e30b2ba-f276-471b-a5e6-14e64de59c1a>
2018-12-12 20:23:03 +0000

    New CAT diameter-based tree logger

--- trunk/src/lerfob/carbonbalancetool/catdiameterbasedtreelogger/CATDiameterBasedTreeLogger.java
+++ trunk/src/lerfob/carbonbalancetool/catdiameterbasedtreelogger/CATDiameterBasedTreeLogger.java
@@ -1,7 +1,26 @@
+/*
+ * This file is part of the lerfob-forestools library.
+ *
+ * Copyright (C) 2010-2018 Mathieu Fortin 
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed with the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * Please see the license at http://www.gnu.org/copyleft/lesser.html.
+ */
 package lerfob.carbonbalancetool.catdiameterbasedtreelogger;
 
 import lerfob.treelogger.diameterbasedtreelogger.DiameterBasedTreeLogger;
 
 public class CATDiameterBasedTreeLogger extends DiameterBasedTreeLogger {
 
+	// TODO FP implement the algorithm for the logging defaultspecies enum should be used by default
 }
