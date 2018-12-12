416b64e4c0fa1bbe4461e0a8a3bdfaa641c10e4b
Mathieu Fortin <50317186+donenrique24@users.noreply.github.com>
2018-12-12 20:23:03 +0000

    New CAT diameter-based tree logger

--- src/lerfob/carbonbalancetool/catdiameterbasedtreelogger/CATDiameterBasedTreeLoggerParametersDialog.java
+++ src/lerfob/carbonbalancetool/catdiameterbasedtreelogger/CATDiameterBasedTreeLoggerParametersDialog.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of the lerfob-forestools library.
+ *
+ * Copyright (C) 2010-2018 Mathieu Fortin 
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed with the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * Please see the license at http://www.gnu.org/copyleft/lesser.html.
+ */
 package lerfob.carbonbalancetool.catdiameterbasedtreelogger;
 
 import java.awt.Window;
@@ -9,15 +27,36 @@ import javax.swing.JOptionPane;
 
 import lerfob.carbonbalancetool.CATSettings.CATSpecies;
 import lerfob.treelogger.diameterbasedtreelogger.DiameterBasedTreeLoggerParametersDialog;
+import repicea.gui.UIControlManager;
 import repicea.simulation.treelogger.TreeLoggerAWTProperty;
+import repicea.simulation.treelogger.TreeLoggerParametersDialog;
 import repicea.util.REpiceaTranslator;
+import repicea.util.REpiceaTranslator.TextableEnum;
 
 @SuppressWarnings("serial")
 public class CATDiameterBasedTreeLoggerParametersDialog extends DiameterBasedTreeLoggerParametersDialog {
 
+	static {
+		UIControlManager.setTitle(CATDiameterBasedTreeLoggerParametersDialog.class, "CAT Tree Logger based on diameter at breast height", "Module de billonnage de CAT bas\u00E9 sur le diam\u00E8tre \u00EA 1,3 m");
+	}
+
 	
-	// TODO avoid deleting the default species
-	
+	private static enum MessageID implements TextableEnum {
+		PleaseChooseOneSpecies("Please select a species in this list:", "Veuillez s\u00E9lectionner une esp\u00E8ce dans cette liste :"),
+		ImpossibleToAddSpecies("There is no species left that can be added to the list!", "Il ne reste plus d'esp\u00E8ce pouvant \00EAtre ajout\u00E9e \u00E0 la liste!");
+
+		MessageID(String englishText, String frenchText) {
+			setText(englishText, frenchText);
+		}
+		
+		@Override
+		public void setText(String englishText, String frenchText) {
+			REpiceaTranslator.setString(this, englishText, frenchText);
+		}
+
+		@Override
+		public String toString() {return REpiceaTranslator.getString(this);}
+	}
 	
 	protected CATDiameterBasedTreeLoggerParametersDialog(Window window, CATDiameterBasedTreeLoggerParameters params) {
 		super(window, params);
@@ -54,15 +93,20 @@ public class CATDiameterBasedTreeLoggerParametersDialog extends DiameterBasedTre
 	@Override
 	protected void speciesAddAction() {
 		CATSpecies newSpecies;
-		List<CATSpecies> availableSpecies = Arrays.asList(CATSpecies.values());
+		List<CATSpecies> availableSpecies = new ArrayList<CATSpecies>();
+		availableSpecies.addAll(Arrays.asList(CATSpecies.values()));
 		availableSpecies.removeAll(retrieveCATSpeciesInCurrentList());
 		if (availableSpecies.isEmpty()) {
+			JOptionPane.showMessageDialog(this, 
+					REpiceaTranslator.getString(MessageID.ImpossibleToAddSpecies), 
+					REpiceaTranslator.getString(TreeLoggerParametersDialog.MessageID.AddANewSpecies),
+					JOptionPane.INFORMATION_MESSAGE);
 			// TODO FP display a message here
 			return;
 		} else {
 			newSpecies = (CATSpecies) JOptionPane.showInputDialog(this, 
-					REpiceaTranslator.getString(MessageID.PleaseEnterTheSpeciesCode),
-					REpiceaTranslator.getString(MessageID.AddANewSpecies),
+					REpiceaTranslator.getString(MessageID.PleaseChooseOneSpecies),
+					REpiceaTranslator.getString(TreeLoggerParametersDialog.MessageID.AddANewSpecies),
 					JOptionPane.QUESTION_MESSAGE,
 					null,
 					availableSpecies.toArray(),
