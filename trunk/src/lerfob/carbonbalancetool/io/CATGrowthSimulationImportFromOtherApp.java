ee37ab18a004e98f4ce1e1ea1b99a83cd0728c75
donenrique <donenrique@3e30b2ba-f276-471b-a5e6-14e64de59c1a>
2019-01-18 09:06:44 +0000

    New class for import directly from R

---
+++ trunk/src/lerfob/carbonbalancetool/io/CATGrowthSimulationImportFromOtherApp.java
@@ -0,0 +1,68 @@
+/*
+ * This file is part of the lerfob-foresttools library.
+ *
+ * Copyright (C) 2010-2019 Mathieu Fortin for LERFOB AgroParisTech/INRA, 
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed with the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * Please see the license at http://www.gnu.org/copyleft/lesser.html.
+ */
+package lerfob.carbonbalancetool.io;
+
+import java.util.List;
+
+import lerfob.carbonbalancetool.CATCompatibleStand;
+import lerfob.carbonbalancetool.CATSettings.CATSpecies;
+import repicea.gui.components.REpiceaMatchSelector;
+import repicea.simulation.covariateproviders.treelevel.TreeStatusProvider.StatusClass;
+
+/**
+ * The CATGrowthSimulationImportFromOtherApp class makes it possible to import 
+ * data from other applications. While the CATGrowthSimulationRecordReader class assumes that
+ * the data come from a file, this class assumes the data are already formatted. 
+ * 
+ * @author Mathieu Fortin - January 2019
+ *
+ */
+public class CATGrowthSimulationImportFromOtherApp {
+
+	private final CATGrowthSimulationRecordReader recordReader;
+	
+	
+	public CATGrowthSimulationImportFromOtherApp() {
+		recordReader = new CATGrowthSimulationRecordReader();
+	}
+	
+
+	public void instantiatePlotAndTree(String standIdentification, int dateYr, int realization, String plotID, double plotAreaHa,
+			StatusClass statusClass, double treeVolumeDm3, double numberOfTrees, String originalSpeciesName, Double dbhCm) {
+		recordReader.instantiatePlotAndTree(standIdentification, dateYr, realization, plotID, plotAreaHa, statusClass, treeVolumeDm3, numberOfTrees, originalSpeciesName, dbhCm);
+	}
+	
+	/**
+	 * This method returns the stand list that was last read.
+	 * @return a list of CATCompatibleStand instances
+	 */
+	public List<CATCompatibleStand> getStandList() {
+		return recordReader.getStandList();
+	}	
+	
+	/**
+	 * This method returns the selector for the species in CAT.
+	 * @return a REpiceaMatchSelector instance
+	 */
+	public REpiceaMatchSelector<CATSpecies> getSelector() {
+		return recordReader.getSelector();
+	}
+
+	
+}
