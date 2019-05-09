4e28d7aae3bfb3d96c2f2ca72108c3d3bca22cf8
Mathieu Fortin <50317186+donenrique24@users.noreply.github.com>
2019-05-09 11:27:42 +0000

    Some export format in CAT now have species breakdown

--- src/lerfob/carbonbalancetool/productionlines/ProductionLineManager.java
+++ src/lerfob/carbonbalancetool/productionlines/ProductionLineManager.java
@@ -404,9 +404,14 @@ public final class ProductionLineManager extends AbstractDesigner<ProductionLine
 	
 	private void actualizeCarbonUnitsOfThisType(CarbonUnitStatus type, CATCompartmentManager compartmentManager) throws Exception {
 		try {
-			for (CarbonUnit carbonUnit : getCarbonUnits(type)) {
+			CarbonUnitList carbonUnitList = getCarbonUnits(type);
+			for (int i = 0; i < carbonUnitList.size(); i++) {
+				CarbonUnit carbonUnit = carbonUnitList.get(i);
 				carbonUnit.actualizeCarbon(compartmentManager);
 			}
+//			for (CarbonUnit carbonUnit : getCarbonUnits(type)) {
+//				carbonUnit.actualizeCarbon(compartmentManager);
+//			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new Exception("An exception occurred while actualizing carbon units of type " + type.name() + " : " + e.getMessage());
