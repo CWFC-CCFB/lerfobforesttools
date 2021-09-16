0876d2261d6ae983e7c054f21d2fe86bbf5b1cf9
jflavoie <jflavoie@3e30b2ba-f276-471b-a5e6-14e64de59c1a>
2021-09-16 19:14:18 +0000

    Added gradle build support

---
+++ trunk/test/lerfob/ProductionEnvironmentTest.java
@@ -0,0 +1,19 @@
+package lerfob;
+import org.junit.Assert;
+import org.junit.Test;
+
+import lerfob.app.LerfobForesttools;
+
+
+public class ProductionEnvironmentTest {
+	@Test 
+	
+	/**
+	 * This test will fail when used with a local class folder, but will succeed when running on a JAR file.
+	 * This test is excluded from the normal local tests and executed only with the integration tests from the JAR file
+	 */
+	public void makeSurePackageIsRunningFromJAR() {
+		String resourceURL = LerfobForesttools.class.getResource("LerfobForesttools.class").toString();
+		Assert.assertTrue(resourceURL.startsWith("jar:"));
+	}
+}
