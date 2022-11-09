0fb062d5e3caf0af7d45e5623905b9b9e37d08a0
donenrique <donenrique@3e30b2ba-f276-471b-a5e6-14e64de59c1a>
2022-11-09 17:52:36 +0000

    Setting appropriate language in two test classes Adding file header

--- trunk/src/lerfob/treelogger/diameterbasedtreelogger/DiameterBasedTreeLoggerParametersDialog.java
+++ trunk/src/lerfob/treelogger/diameterbasedtreelogger/DiameterBasedTreeLoggerParametersDialog.java
@@ -1,12 +1,12 @@
 /*
- * This file is part of the repicea library.
+ * This file is part of the lerfob-forestools library.
  *
- * Copyright (C) 2009-2015 Mathieu Fortin for Rouge-Epicea
+ * Copyright (C) 2010-2012 Mathieu Fortin for LERFOB INRA/AgroParisTech, 
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * This library is distributed with the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied
